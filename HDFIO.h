64dafad88ae14d7ded6f4a09330e46883887ead1
deskinsjt <jtd55@case.edu>
2016-05-09 19:25:05 -0400

    Some updates to datatypes and a start on the read function.

--- HDFIO.h
+++ HDFIO.h
@@ -17,6 +17,7 @@
 #define HDFIO_DEFAULT_CHUNK_DIMS 8
 #define HDFIO_DEFAULT_BLOCK_DIMS 1
 #define HDFIO_DEFAULT_START_POINT 0
+#define HDFIO_DEFAULT_STRIDE 1
 
 #define HDFIO_VERBOSE_COUT if(verbosity_level == HDFIO_VERBOSE_ON) std::cout
 #define HDFIO_DEBUG_COUT if(verbosity_level >= HDFIO_VERBOSE_ON) std::cout
@@ -30,11 +31,11 @@ struct HDFIOFileInfo
           data_compression_plist;
 };
 
-template <typename RT, typename IT>
+//template <typename RT, typename IT>
 class HDFIO
 {
 private:
-  IT rank;
+  int rank;
 
   hsize_t *mem_dims,
           *stride,
@@ -44,7 +45,8 @@ private:
           *start_point,
           *chunk_dims;
 
-  hid_t output_type,
+  hid_t dataset_type,
+        memory_type,
         mem_dspace_id; // mem dataspace identifier
   
   herr_t  status;
@@ -54,30 +56,29 @@ private:
 
 
 public:
-  HDFIO(hsize_t *mem_dims_in, IT rank_in)
+  HDFIO(hsize_t *mem_dims_in, int rank_in, hid_t memory_type_in)
   {
-    initialize(mem_dims_in, rank_in, HDFIO_VERBOSE_OFF);
+    initialize(mem_dims_in, rank_in, memory_type_in, HDFIO_VERBOSE_OFF);
   }
 
-  HDFIO(hsize_t *mem_dims_in, IT rank_in, int verbosity_level_in)
+  HDFIO(hsize_t *mem_dims_in, int rank_in, hid_t memory_type_in, int verbosity_level_in)
   {
-    initialize(mem_dims_in, rank_in, verbosity_level_in);
+    initialize(mem_dims_in, rank_in, memory_type_in, verbosity_level_in);
   }
 
-  void initialize(hsize_t *mem_dims_in, IT rank_in, int verbosity_level_in)
+  void initialize(hsize_t *mem_dims_in, int rank_in, hid_t memory_type_in, int verbosity_level_in)
   {
 
     verbosity_level = verbosity_level_in;
     compression_level = 9;
-    output_type = H5T_NATIVE_FLOAT;
+    memory_type = memory_type_in;
+    dataset_type = memory_type;
 
     rank = rank_in;
 
     HDFIO_VERBOSE_COUT << "Creating HDFIO with rank = " << rank << std::endl << std::flush;
-    HDFIO_DEBUG_COUT << "sizeof(hsize_t) = " << sizeof(hsize_t) << std::endl << std::flush;
-    HDFIO_DEBUG_COUT << "sizeof(mem_dims_in) = " << sizeof(mem_dims_in) << std::endl << std::flush;
 
-    HDFIO_DEBUG_COUT << "Allocating internal variables..." << std::endl << std::flush;
+    HDFIO_DEBUG_COUT << "Allocating internal variables..." << std::flush;
     mem_dims = new hsize_t[rank];
     stride = new hsize_t[rank];
     file_dims = new hsize_t[rank];
@@ -85,24 +86,28 @@ public:
     block_dims = new hsize_t[rank];
     chunk_dims = new hsize_t[rank];
     start_point = new hsize_t[rank];
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
 
-    HDFIO_DEBUG_COUT << "Initializing mem_dims..." << std::endl << std::flush;
-    for(IT i=0; i<rank; ++i)
+    HDFIO_DEBUG_COUT << "Initializing mem_dims..." << std::flush;
+    for(int i=0; i<rank; ++i)
     {
       mem_dims[i] = mem_dims_in[i];
     }
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
 
     /* Create dataspace for the memory array */
-    HDFIO_DEBUG_COUT << "Creating mem_dspace_id..." << std::endl << std::flush;
+    HDFIO_DEBUG_COUT << "Creating mem_dspace_id..." << std::flush;
     mem_dspace_id = H5Screate_simple(rank, mem_dims, NULL);
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
 
-    setHyperslabParameters(2);
+    setHyperslabParameters(HDFIO_DEFAULT_STRIDE);
   }
 
   ~HDFIO()
   {
-    HDFIO_DEBUG_COUT << "Closing HDFIO..." << std::endl << std::flush;
-    status = H5Sclose(mem_dspace_id); 
+    HDFIO_DEBUG_COUT << "Closing HDFIO..." << std::flush;
+    status = H5Sclose(mem_dspace_id);
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
   }
 
   /**
@@ -119,23 +124,29 @@ public:
     verbosity_level = verbosity_in;
   }
 
-  void setOutputType(hid_t output_type_in)
+  void setDatasetType(hid_t dataset_type_in)
   {
-    output_type = output_type_in;
+    dataset_type = dataset_type_in;
+  }
+
+  void setMemoryType(hid_t memory_type_in)
+  {
+    memory_type = memory_type_in;
   }
 
   void setHyperslabParameters(hsize_t stride_in)
   {
-    HDFIO_DEBUG_COUT << "Setting stride..." << std::endl << std::flush;
-    for(IT i=0; i<rank; ++i)
+    HDFIO_DEBUG_COUT << "Setting stride..." << std::flush;
+    for(int i=0; i<rank; ++i)
       stride[i] = stride_in;
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
     setHyperslabParameters(stride);
   }
 
   void setHyperslabParameters(hsize_t *stride_in)
   {
-    HDFIO_DEBUG_COUT << "Setting additional hyperslab parameters..." << std::endl << std::flush;
-    for(IT i=0; i<rank; ++i)
+    HDFIO_DEBUG_COUT << "Setting additional hyperslab parameters..." << std::flush;
+    for(int i=0; i<rank; ++i)
     {
       stride[i] = stride_in[i]; // stride of hyperslab
       file_dims[i] = mem_dims[i]/stride[i];
@@ -144,6 +155,7 @@ public:
       block_dims[i] = HDFIO_DEFAULT_BLOCK_DIMS; // size of blocks (chosen to be the size of the file's dataset)
       start_point[i] = HDFIO_DEFAULT_START_POINT; // start of hyperslab
     }
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
 
     HDFIO_DEBUG_COUT << "Selecting hyperslab..." << std::flush;
         /* Select hyperslab for memory array to be output */
@@ -196,7 +208,7 @@ public:
 
     /* Create dataset and write it into the file */
     HDFIO_DEBUG_COUT << "Creating dataset..." << std::flush;
-    file_info.dataset_id = H5Dcreate(file_info.file_id, dataset_name.c_str(), output_type, file_info.file_dspace_id, H5P_DEFAULT, file_info.data_compression_plist, H5P_DEFAULT);
+    file_info.dataset_id = H5Dcreate(file_info.file_id, dataset_name.c_str(), dataset_type, file_info.file_dspace_id, H5P_DEFAULT, file_info.data_compression_plist, H5P_DEFAULT);
     HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
       
     return file_info;
@@ -210,18 +222,26 @@ public:
     status = H5Pclose(file_info->data_compression_plist);
   }
 
-  void writeArrayToFile(RT array[], std::string file_name, std::string dataset_name)
-  {
-      
+  void writeArrayToFile(void *array, std::string file_name, std::string dataset_name)
+  {  
     HDFIOFileInfo file_info = getFileInfo(file_name,dataset_name);
 
     /* Write the data */
     HDFIO_DEBUG_COUT << "Writing dataset..." << std::flush;
-    status = H5Dwrite(file_info.dataset_id, output_type, mem_dspace_id, file_info.file_dspace_id, H5P_DEFAULT, array);
+    status = H5Dwrite(file_info.dataset_id, memory_type, mem_dspace_id, file_info.file_dspace_id, H5P_DEFAULT, array);
     HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
+   
+    closeFileInfo(&file_info);
+  }
 
-    /* Reset the selection for the memory dataspace fid */
-    //status = H5Sselect_none(mem_dspace_id);
+  void readFileToArray(void *array, std::string file_name, std::string dataset_name)
+  {  
+    HDFIOFileInfo file_info = getFileInfo(file_name,dataset_name);
+
+    /* Write the data */
+    HDFIO_DEBUG_COUT << "Writing dataset..." << std::flush;
+    status = H5Dread(file_info.dataset_id, memory_type, mem_dspace_id, file_info.file_dspace_id, H5P_DEFAULT, array);
+    HDFIO_DEBUG_COUT << "Done!" << std::endl << std::flush;
    
     closeFileInfo(&file_info);
   }
--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
-	float f[2000*1000];
-	for(int i = 0; i<2000*1000; ++i)
+	#define ARRAY_RANK 2
+	hsize_t dims[ARRAY_RANK] = {2000,1000};
+	hsize_t gridsize=1;
+	for(int i = 0; i<ARRAY_RANK; ++i)
+		gridsize*=dims[i];
+	float f[gridsize];
+	for(hsize_t i = 0; i<gridsize; ++i)
 		f[i]=rand()/((float) RAND_MAX);
 	std::cout << f[0] <<std::endl << std::flush;
-	hsize_t dims[2] = {2000,1000};
-	HDFIO<float,int> myIO(dims, 2, HDFIO_VERBOSE_DEBUG);
+	HDFIO myIO(dims, ARRAY_RANK, H5T_NATIVE_FLOAT, HDFIO_VERBOSE_DEBUG);
+	myIO.setDatasetType(H5T_NATIVE_DOUBLE);
 	myIO.setHyperslabParameters(1);
 	myIO.writeArrayToFile(f,"test.h5.gz","data1");
 	return 0;
